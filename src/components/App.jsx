da92f17c708985869a1efc006c09a9c0b54e4946
Juan Blanco <juanblancodev@gmail.com>
2024-03-28 02:14:02 -0700

    container styled added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,20 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 680px;
+  height: 200px;
+  max-width: 1440px;
+  min-height: 700px;
+  padding: 30px;
+  margin: 100px auto;
+  background-color: var(--color-white); 
+  border-radius: 15px;
+`
+
 const App = () => {
   return (
-    <div>App</div>
+    <Container>
+    </Container>
   )
 }
 
